303740091845548f1081ab0232529290acb7aeed
yongqiang <yongqiang@futunn.com>
2017-08-31 16:02:24 +0800

    [add]

--- app.js
+++ app.js
@@ -22,6 +22,55 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ---begin test region---
+// app.use(function (req, res, next) {
+//   console.log('Time:', Date.now());
+//   next();
+// });
+
+
+// app.get('/user/:id', function (req, res, next) {
+//   console.log('ID:', req.params.id);
+//   next();
+// }, function (req, res, next) {
+//   res.send('User Info');
+//   next();
+// });
+
+
+// app.use('/user/:id', function(req, res, next) {
+//   console.log('Request URL:', req.originalUrl);
+//   next();
+// }, function (req, res, next) {
+//   console.log('Request Type:', req.method);
+//   next();
+// });
+
+// handler for the /user/:id path, which prints the user ID
+// app.get('/user/:id', function (req, res, next) {
+//   res.end(req.params.id);
+// });
+
+
+// app.get('/user/:id', function (req, res, next) {
+//   // if the user ID is 0, skip to the next route
+//   if (req.params.id == 0) next('route');
+//   // otherwise pass the control to the next middleware function in this stack
+//   else next(); //
+// }, function (req, res, next) {
+//   // render a regular page
+//   res.render('regular');
+// });
+
+// // handler for the /user/:id path, which renders a special page
+// app.get('/user/:id', function (req, res, next) {
+//   res.render('special');
+// });
+
+
+
+// ---end test region---
+
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+
+
+
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next();
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	// console.log('Time1:', Date.now());
   // res.render('index', { title: 'Express' });
   res.sendFile(path.resolve('public/html/index.html'));
 });
